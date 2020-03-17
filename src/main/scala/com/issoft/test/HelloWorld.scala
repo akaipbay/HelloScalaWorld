b4ffc6c316088be34465bba445760299e61677ac
akaipbay <akaipbay@gmail.com>
2020-03-17 17:03:51 +0300

    Added new another fixed feature

--- src/main/scala/com/issoft/test/HelloWorld.scala
+++ src/main/scala/com/issoft/test/HelloWorld.scala
@@ -5,7 +5,7 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello World!!!")
 
-    println("New feuature was added!!!")
+    println("New feature was added!!!")
   }
 
 }
